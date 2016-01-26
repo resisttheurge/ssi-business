3df7b5476aa8dfbde2621a57275eef4603aa1b13
Jurbin6090 <jurbin6090@gmail.com>
2016-01-26 14:03:37 -0600

    Added extra layouts and updated classes

---
+++ reporting/src/com/cooksys/model/ManagementReview.java
@@ -0,0 +1,223 @@
+package com.cooksys.model;
+
+import static com.cooksys.util.DataUtil.convertHeaderYear;
+import static com.cooksys.util.DataUtil.convertRaw;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.cooksys.test.VariableGenerator;
+
+public class ManagementReview implements VariableGenerator<ManagementReview>
+{
+
+	String prefix;
+	String year;
+	String label;
+	String customer;
+	String city;
+	String state;
+	String startDate;
+	String dueDate;
+	String completeDate;
+	String status;
+	String description;
+	
+	static int onTimeOrders;
+	static int lateOrders;
+	static int noStatusOrders;
+	
+	@Override
+	public List<ManagementReview> generateVariables(ResultSet rawData)
+	{
+		ArrayList<ManagementReview> mgmtList = new ArrayList<ManagementReview>();
+
+		try
+		{
+			while (rawData.next())
+			{
+				ManagementReview mgmtReview = new ManagementReview();
+				
+				mgmtReview.setPrefix(convertRaw(rawData.getString(1)));
+				mgmtReview.setYear(convertHeaderYear(rawData.getDate(2)));
+				mgmtReview.setLabel(convertRaw(rawData.getString(3)));
+				mgmtReview.setCustomer(convertRaw(rawData.getString(4)));
+				mgmtReview.setCity(convertRaw(rawData.getString(5)));
+				mgmtReview.setState(convertRaw(rawData.getString(6)));
+				mgmtReview.setStartDate(convertRaw(rawData.getDate(7)));
+				
+				Date dueDate = rawData.getDate(8);
+				
+				mgmtReview.setDueDate(convertRaw(dueDate));
+				
+				Date completeDate = rawData.getDate(9);
+				
+				mgmtReview.setCompleteDate(convertRaw(completeDate));
+				mgmtReview.setStatus(convertRaw(rawData.getString(10)));
+				mgmtReview.setDescription(convertRaw(rawData.getString(11)));
+				
+				tallyOrders(dueDate, completeDate);
+				
+				mgmtList.add(mgmtReview);
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return mgmtList;
+	}
+
+	private void tallyOrders(Date dueDate, Date completeDate)
+	{
+		if(completeDate == null)
+			noStatusOrders++;
+		else if(dueDate.getTime() >= completeDate.getTime())
+			onTimeOrders++;
+		else
+			lateOrders++;	
+	}
+
+	public String getPrefix()
+	{
+		return prefix;
+	}
+
+	public void setPrefix(String prefix)
+	{
+		this.prefix = prefix;
+	}
+
+	public String getYear()
+	{
+		return year;
+	}
+
+	public void setYear(String year)
+	{
+		this.year = year;
+	}
+
+	public String getLabel()
+	{
+		return label;
+	}
+
+	public void setLabel(String label)
+	{
+		this.label = label;
+	}
+
+	public String getCustomer()
+	{
+		return customer;
+	}
+
+	public void setCustomer(String customer)
+	{
+		this.customer = customer;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+	public String getStartDate()
+	{
+		return startDate;
+	}
+
+	public void setStartDate(String startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	public String getDueDate()
+	{
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate)
+	{
+		this.dueDate = dueDate;
+	}
+
+	public String getCompleteDate()
+	{
+		return completeDate;
+	}
+
+	public void setCompleteDate(String completeDate)
+	{
+		this.completeDate = completeDate;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public int getOnTimeOrders()
+	{
+		return onTimeOrders;
+	}
+
+	public void setOnTimeOrders(int onTimeOrders)
+	{
+		ManagementReview.onTimeOrders = onTimeOrders;
+	}
+
+	public int getLateOrders()
+	{
+		return lateOrders;
+	}
+
+	public void setLateOrders(int lateOrders)
+	{
+		ManagementReview.lateOrders = lateOrders;
+	}
+
+	public int getNoStatusOrders()
+	{
+		return noStatusOrders;
+	}
+
+	public void setNoStatusOrders(int noStatusOrders)
+	{
+		ManagementReview.noStatusOrders = noStatusOrders;
+	}
+
+}
---
+++ reporting/src/com/cooksys/model/MaterialShipperShippingGroupItem.java
@@ -0,0 +1,86 @@
+package com.cooksys.model.materialShipper;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import com.cooksys.test.VariableGenerator;
+
+public class MaterialShipperShippingGroupItem implements VariableGenerator<MaterialShipperShippingGroupItem>{
+
+	private String markId;
+	private String mark;
+	private String jobDesc;
+	private String shop;
+	private String requested;
+	private String quantity;
+	private String completed;
+	private String status;
+	
+	ArrayList<MaterialShipperZone> zoneList = new ArrayList<MaterialShipperZone>();
+	
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public String getJobDesc() {
+		return jobDesc;
+	}
+	public void setJobDesc(String jobDesc) {
+		this.jobDesc = jobDesc;
+	}
+	public String getShop() {
+		return shop;
+	}
+	public void setShop(String shop) {
+		this.shop = shop;
+	}
+	public String getRequested() {
+		return requested;
+	}
+	public void setRequested(String requested) {
+		this.requested = requested;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getCompleted() {
+		return completed;
+	}
+	public void setCompleted(String completed) {
+		this.completed = completed;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getBalance() {
+		return String.valueOf(NumberUtils.toInt(requested) - NumberUtils.toInt(quantity));
+	}
+	public ArrayList<MaterialShipperZone> getZoneList() {
+		return zoneList;
+	}
+	public void setZoneList(ArrayList<MaterialShipperZone> zoneList) {
+		this.zoneList = zoneList;
+	}
+	@Override
+	public List<MaterialShipperShippingGroupItem> generateVariables(ResultSet rawData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getMarkId() {
+		return markId;
+	}
+	public void setMarkId(String markId) {
+		this.markId = markId;
+	}
+}
