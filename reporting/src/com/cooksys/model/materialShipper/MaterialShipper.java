a818bd5cf40dbd15aa1b0b38f1122f6c9f4c49bf
Jurbin6090 <jurbin6090@gmail.com>
2016-01-26 13:52:22 -0600

    Added extra layouts and updated classes

--- reporting/src/com/cooksys/model/materialShipper/MaterialShipper.java
+++ reporting/src/com/cooksys/model/materialShipper/MaterialShipper.java
@@ -1,24 +1,26 @@
 package com.cooksys.model.materialShipper;
 
+import static com.cooksys.util.DataUtil.convertRaw;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.cooksys.db.Connector;
-import com.cooksys.model.LayoutDrawing;
 import com.cooksys.test.VariableGenerator;
-
-import static com.cooksys.util.DataUtil.*;
 
 public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 
 	private String jobId;
 	private String jobName;
 	private String jobNumber;
-
+	private ResultSet zoneList;
 	private ArrayList<MaterialShipperDrawing> drawingList = new ArrayList<MaterialShipperDrawing>();
+	private ArrayList<MaterialShipperShippingGroup> shippingGroupList = new ArrayList<MaterialShipperShippingGroup>();
 
 
 	@Override
@@ -40,6 +42,18 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 				ResultSet drawingSet = Connector.executeStoredProcedure(new MaterialShipperDrawing(),
 						new String[] { item.jobId });
 
+				ResultSet zoneListSet = Connector.executeStoredProcedure("ZoneList", new String[] { item.jobId});
+				HashSet<MaterialShipperZone> zoneBaseSet = new HashSet<MaterialShipperZone>();
+				
+				while(zoneListSet.next())
+				{
+					MaterialShipperZone zone = new MaterialShipperZone();
+					zone.setNumber(zoneListSet.getString(1));
+					zone.setQuantity(String.valueOf(0));
+					
+					zoneBaseSet.add(zone);
+				}
+				
 				while(drawingSet.next())
 				{
 					MaterialShipperDrawing drawing = new MaterialShipperDrawing();
@@ -69,6 +83,9 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 						ResultSet zoneSet = Connector.executeStoredProcedure(new MaterialShipperZone(),
 								new String[] { mark.getMarkId() });
 						
+						HashSet<MaterialShipperZone> cloneZone = (HashSet<MaterialShipperZone>) zoneBaseSet.clone();
+						
+						
 						while(zoneSet.next())
 						{
 							MaterialShipperZone zone = new MaterialShipperZone();
@@ -76,9 +93,11 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 							zone.setNumber(convertRaw(zoneSet.getString(1)));
 							zone.setQuantity(convertRaw(zoneSet.getString(2)));
 							
-							mark.getZoneList().add(zone);
-							
+							cloneZone.remove(zone);
+							cloneZone.add(zone);		
 						}
+						
+						mark.getZoneList().addAll(cloneZone);
 						
 						Collections.sort(mark.getZoneList());
 						
@@ -87,6 +106,65 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 					
 					item.drawingList.add(drawing);
 				}
+				
+				
+				
+				ResultSet shippingGroupSet = Connector.executeStoredProcedure("MaterialShipperShippingGroup",
+						new String[] { item.jobId });
+
+				while(shippingGroupSet.next())
+				{
+					MaterialShipperShippingGroup shippingGroup = new MaterialShipperShippingGroup();
+					
+					shippingGroup.setDrawing(convertRaw(shippingGroupSet.getString(1)));
+					shippingGroup.setRevision(convertRaw(shippingGroupSet.getString(2)));
+					shippingGroup.setDrawingDesc(convertRaw(shippingGroupSet.getString(3)));
+					shippingGroup.setRevisionDate(convertRaw(shippingGroupSet.getDate(4)));					
+					shippingGroup.setDrawingId(convertRaw(shippingGroupSet.getString(5)));
+							
+					ResultSet markSet = Connector.executeStoredProcedure(new MaterialShipperShippingGroupItem(),
+							new String[] { shippingGroup.getDrawingId() });
+					
+					while(markSet.next())
+					{
+						MaterialShipperShippingGroupItem shippingGroupItem = new MaterialShipperShippingGroupItem();
+						
+						shippingGroupItem.setMarkId(convertRaw(markSet.getString(1)));
+						shippingGroupItem.setMark(convertRaw(markSet.getString(2)));
+						shippingGroupItem.setJobDesc(convertRaw(markSet.getString(3)));
+						shippingGroupItem.setShop(convertRaw(markSet.getString(4)));
+						shippingGroupItem.setRequested(convertRaw(markSet.getString(5)));
+						shippingGroupItem.setQuantity(convertRaw(markSet.getString(6)));
+						shippingGroupItem.setCompleted(convertRaw(markSet.getString(7)));
+						shippingGroupItem.setStatus(convertRaw(markSet.getString(8)));
+						
+						ResultSet zoneList = getZoneList(shippingGroupItem);
+						
+						HashSet<MaterialShipperZone> cloneZone = (HashSet<MaterialShipperZone>) zoneBaseSet.clone();
+						
+						
+						if(zoneList.next())
+						{
+							MaterialShipperZone zone = new MaterialShipperZone();
+						
+							zone.setNumber(convertRaw(zoneList.getString(1)));
+							zone.setQuantity(convertRaw(zoneList.getString(2)));
+							
+							cloneZone.remove(zone);
+							cloneZone.add(zone);
+							
+						}
+						
+						shippingGroupItem.getZoneList().addAll(cloneZone);
+						
+						Collections.sort(shippingGroupItem.getZoneList());
+						
+						shippingGroup.getShippingGroupItemList().add(shippingGroupItem);
+					}
+					
+					item.shippingGroupList.add(shippingGroup);
+				}
+				
 				
 				//Collections.sort(item.drawingList);
 			}
@@ -97,6 +175,19 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 		}
 
 		return result;
+	}
+
+
+	private ResultSet getZoneList(MaterialShipperShippingGroupItem shippingGroupItem) {
+		try {
+			if(zoneList == null || zoneList.isLast())
+				zoneList = Connector.executeStoredProcedure(new MaterialShipperZone(),
+					new String[] { shippingGroupItem.getMarkId() });
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return zoneList;
 	}
 
 
@@ -137,5 +228,15 @@ public class MaterialShipper implements VariableGenerator<MaterialShipper> {
 
 	public void setDrawingList(ArrayList<MaterialShipperDrawing> drawingList) {
 		this.drawingList = drawingList;
+	}
+
+
+	public ArrayList<MaterialShipperShippingGroup> getShippingGroupList() {
+		return shippingGroupList;
+	}
+
+
+	public void setShippingGroupList(ArrayList<MaterialShipperShippingGroup> shippingGroupList) {
+		this.shippingGroupList = shippingGroupList;
 	}
 }
