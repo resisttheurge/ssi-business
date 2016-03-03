f6dcc49534029d18d460b01b72c6c0a896557f06
Peter Zastoupil <peterzastoupil@gmail.com>
2016-03-03 12:28:04 -0600

    extracted jobService.js

---
+++ resources/ui/app/js/services/api/jobService.js
@@ -0,0 +1,35 @@
+'use strict'
+
+angular.module('ssiServices')
+  .service('Job', ['$resource', 'endpointUrl',
+    function($resource, endpointUrl) {
+
+      var service = this;
+
+      var resultExtension = function(response){
+        service.$scope.loading = true
+        if(response.success) {
+          angular.extend(service.resultObj, response.data);
+        } else {
+          service.$scope.error = true
+          service.$scope.message = response.message
+        }
+        service.$scope.loading = false
+      }
+
+
+       this.endpoint = $resource(endpointUrl + '/jobs/:jobId', {}, {
+        create: {method: 'POST'},
+        update: {method: 'PATCH'},
+        query: {method: 'GET', params:{jobId: ''}}
+      });
+
+      this.get = function($scope, resultObj, jobId)
+      {
+        service.$scope = $scope;
+        service.resultObj = resultObj;
+        return this.endpoint.get({'jobId' : jobId}, resultExtension).$promise;
+      }
+
+    }
+  ])
--- resources/ui/app/js/services/apiServices.js
+++ resources/ui/app/js/services/apiServices.js
@@ -111,39 +111,6 @@ apiServices.factory('MarkByDrawing', ['$resource', 'endpointUrl',
   }
 ])
 
-apiServices.service('Job', ['$resource', 'endpointUrl',
-  function($resource, endpointUrl) {
-
-    var service = this;
-
-    var resultExtension = function(response){
-      service.$scope.loading = true
-      if(response.success) {
-        angular.extend(service.resultObj, response.data);
-      } else {
-        service.$scope.error = true
-        service.$scope.message = response.message
-      }
-      service.$scope.loading = false
-    }
-
-
-     this.endpoint = $resource(endpointUrl + '/jobs/:jobId', {}, {
-      create: {method: 'POST'},
-      update: {method: 'PATCH'},
-      query: {method: 'GET', params:{jobId: ''}}
-    });
-
-    this.get = function($scope, resultObj, jobId)
-    {
-      service.$scope = $scope;
-      service.resultObj = resultObj;
-      return this.endpoint.get({'jobId' : jobId}, resultExtension).$promise;
-    }
-
-  }
-])
-
 apiServices.factory('AddendumByJob', ['$resource', 'endpointUrl',
   function($resource, endpointUrl) {
     return $resource(endpointUrl + '/jobs/:jobId/addenda', {}, {
