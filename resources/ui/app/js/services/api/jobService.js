fee98c860623506c69b3566430e66d248784419d
Peter Zastoupil <peterzastoupil@gmail.com>
2016-03-04 17:24:06 -0600

    job detail controller/service integration

--- resources/ui/app/js/services/api/jobService.js
+++ resources/ui/app/js/services/api/jobService.js
@@ -1,34 +1,66 @@
 'use strict'
 angular.module('ssiServices')
-  .service('Job', ['$resource', 'endpointUrl',
-    function($resource, endpointUrl) {
+  .service('Job', [
+    '$q', '$resource', '$unpack', 'endpointUrl', 'JobAddresses', 'JobSchedules',
+    function($q, $resource, $unpack, endpointUrl, JobAddresses, JobSchedules) {
 
-      var service = this;
+      var self = this
 
-      var resultExtension = function(response){
-        service.$scope.loading = true
-        if(response.success) {
-          angular.extend(service.resultObj, response.data);
-        } else {
-          service.$scope.error = true
-          service.$scope.message = response.message
-        }
-        service.$scope.loading = false
-      }
+      self.addresses = JobAddresses
+      self.schedules = JobSchedules
 
-
-       this.endpoint = $resource(endpointUrl + '/jobs/:jobId', {}, {
+      self.endpoint = $resource(endpointUrl + '/jobs/:jobId', {}, {
         create: {method: 'POST'},
         update: {method: 'PATCH'},
         query: {method: 'GET', params:{jobId: ''}}
-      });
+      })
 
-      this.get = function($scope, resultObj, jobId)
-      {
-        service.$scope = $scope;
-        service.resultObj = resultObj;
-        return this.endpoint.get({'jobId' : jobId}, resultExtension).$promise;
+      self.list = function(params) {
+        return $q(function(resolve, reject){
+          return resolve(self.endpoint.query(params).$promise.then($unpack))
+        })
       }
 
+      self.get = function(params) {
+        return $q(function(resolve, reject){
+          return resolve(self.endpoint.get(params).$promise.then($unpack))
+        })
+      }
+
+      self.create = function(job) {
+        return $q(function(resolve, reject){
+          if(!job) {
+            return reject('cannot call `Job.create` without a job parameter')
+          } else if (job.id) {
+            return reject('cannot call `Job.create` on a job object that has an existing `id` value')
+          } else {
+            return resolve(self.endpoint.create(job).$promise.then($unpack))
+          }
+        })
+      }
+
+      self.update = function(job) {
+        return $q(function(resolve, reject){
+          if(!job) {
+            return reject('cannot call `Job.update` without a job parameter')
+          } else if (!job.id) {
+            return reject('cannot call `Job.update` on a job object missing an `id` value')
+          } else {
+            return resolve(self.endpoint.update({jobId: job.id}, job).$promise.then($unpack))
+          }
+        })
+      }
+
+      self.delete = function(job) {
+        return $q(function(resolve, reject){
+          if(!job) {
+            return reject('cannot call `Job.delete` without a job parameter')
+          } else if (!job.id) {
+            return reject('cannot call `Job.delete` on a job object missing an `id` value')
+          } else {
+            return resolve(self.endpoint.delete({jobId: job.id}, job).$promise.then($unpack))
+          }
+        })
+      }
     }
   ])
