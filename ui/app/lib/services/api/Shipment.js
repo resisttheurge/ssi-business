1a17601a558333b2b4a2962c292282ba91a6867e
Dylan Baker <dbrakermusic@gmail.com>
2016-03-26 13:24:40 -0500

    implemented preliminary crud apis for zones, partOrders, shipment, shippingGroupItem, marks

--- ui/app/lib/services/api/Shipment.js
+++ ui/app/lib/services/api/Shipment.js
@@ -12,25 +12,67 @@ export default class Shipment extends ApiService {
         query: { method: 'GET', params:{ shipmentId: '' } }
       })
 
-    self.update = function (item) {
+    //  //
+    // self.update = function (item) {
+    //     return $q(function (resolve, reject) {
+    //       if (!item) {
+    //         return reject('cannot update without a parameter')
+    //       } else if (!item.id) {
+    //         return reject('cannot update object with missing id')
+    //       } else {
+    //         return resolve(self.endpoint.update({ shipmentId: item.id }, item).$promise.then($unpack))
+    //       }
+    //     })
+    //   }
+
+    self.update = function (shipment) {
         return $q(function (resolve, reject) {
-          if (!item) {
+          if (!shipment) {
             return reject('cannot update without a parameter')
-          } else if (!item.id) {
+          } else if (!shipment.id) {
             return reject('cannot update object with missing id')
           } else {
-            return resolve(self.endpoint.update({ shipmentId: item.id }, item).$promise.then($unpack))
+            return resolve(
+              self.endpoint.update({ shipmentId: shipment.id }, shipment).$promise.then($unpack))
           }
         })
       }
 
-    this.create = item =>
-      $q(
-        (resolve, reject) =>
-          item ?
-            resolve(this.endpoint.create(item).$promise.then($unpack))
-          : reject('cannot call create without a parameter')
-      )
+    // //old method
+    // this.create = item =>
+    //   $q(
+    //     (resolve, reject) =>
+    //       item ?
+    //         resolve(this.endpoint.create(item).$promise.then($unpack))
+    //       : reject('cannot call create without a parameter')
+    //   )
 
+    self.create = function (shipment) {
+        return $q(function (resolve, reject) {
+          if (!shipment) {
+            return reject('cannot call `shipment.create` without a shipment parameter')
+          } else {
+            return resolve(shipment =>
+              self.endpoint.create(self.shipmentDateToString(shipment)).$promise
+                .then($unpack)
+            )
+          }
+        })
+      }
+
+    self.delete = function (shipment) {
+        return $q(function (resolve, reject) {
+          if (!shipment) {
+            return reject(
+              'cannot call `shipment.delete` without a shipment parameter')
+          } else if (!shipment.id) {
+            return reject(
+              'cannot call `shipment.delete` on a shipment object missing an `id` value')
+          } else {
+            return resolve(self.endpoint.delete({ shipmentId: shipment.id },
+              self.shipmentDateToString(shipment)).$promise.then($unpack))
+          }
+        })
+      }
   }
 }
