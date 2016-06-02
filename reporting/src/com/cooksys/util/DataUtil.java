0cf46201fe987c66f0f59519d7f93f10297b9f90
Peter Zastoupil <peterzastoupil@gmail.com>
2016-06-02 14:22:49 -0500

    Removing xml illegal characters

--- reporting/src/com/cooksys/util/DataUtil.java
+++ reporting/src/com/cooksys/util/DataUtil.java
@@ -10,14 +10,21 @@ import com.google.common.base.Strings;
 public class DataUtil {
 	private static final SimpleDateFormat mdyFormat = new SimpleDateFormat("MM/dd/yyyy");
 
+	private static final String xml1regex = "[^"
+			+ "\u0009\r\n"
+			+ "\u0020-\uD7FF"
+			+ "\uE000-\uFFFD"
+			+ "\ud800\udc00-\udbff\udfff"
+			+ "]";
+
 	public static String convertRaw(String data){
-		return escapeHtml4(escapeXml10(Strings.nullToEmpty(data)));
+		return escapeHtml4(Strings.nullToEmpty(data)).replaceAll(xml1regex, "");
 	}
 	
 	public static String convertRaw(int data){
 		return convertRaw(String.valueOf(data));
 	}
-	
+
 	public static String convertRaw(Date data){
 		return convertRaw(data == null ? "" : mdyFormat.format(data));
 	}
